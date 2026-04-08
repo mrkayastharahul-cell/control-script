d795441a894aa10e34899a59d0df45864c4ad0ff
mrkayastharahul-cell <mr.kayastharahul@gmail.com>
2026-04-08 06:50:58 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-console.log("Script is LIVE");
-document.body.style.backgroundColor = "black";
+console.log("Automation started");
+
+setInterval(() => {
+  const btn = document.querySelector('button');
+
+  if (btn) {
+    btn.click();
+    console.log("Clicked");
+  } else {
+    console.log("Not found");
+  }
+}, 5000);
